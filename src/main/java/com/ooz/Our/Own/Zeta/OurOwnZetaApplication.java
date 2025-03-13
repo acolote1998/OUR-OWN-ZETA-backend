57f3070cf85d667493acc79c8ab71bef39c4bf18
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-13 23:30:25 +0100

    Initial commit

---
+++ src/main/java/com/ooz/Our/Own/Zeta/OurOwnZetaApplication.java
@@ -0,0 +1,13 @@
+package com.ooz.Our.Own.Zeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurOwnZetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurOwnZetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooz/Our/Own/Zeta/OurOwnZetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooz.Our.Own.Zeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurOwnZetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
