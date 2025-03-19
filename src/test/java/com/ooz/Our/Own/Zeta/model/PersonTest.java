adddf19357baf5064fb0238c6b2176de728bef67
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-19 23:00:35 +0100

    Test: Person passing 20 years

--- src/test/java/com/ooz/Our/Own/Zeta/model/PersonTest.java
+++ src/test/java/com/ooz/Our/Own/Zeta/model/PersonTest.java
@@ -7,15 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonTest {
 
     @Test
-    public void personStartsAtZero(){
+    public void personStartsAtZero() {
         Person person = new Person();
 
-        assertEquals(0,person.getAge());
-        assertEquals("",person.getName());
-        assertEquals("",person.getField());
-        assertTrue(person.getDiscoveries().isEmpty(),"Discoveries is not empty");
-        assertTrue(person.isAlive(),"Person is not alive");
-        assertEquals(0,person.getKnowledge());
+        assertEquals(0, person.getAge());
+        assertEquals("", person.getName());
+        assertEquals("", person.getField());
+        assertTrue(person.getDiscoveries().isEmpty(), "Discoveries is not empty");
+        assertTrue(person.isAlive(), "Person is not alive");
+        assertEquals(0, person.getKnowledge());
+    }
+
+    @Test
+    public void passing20Years() {
+        Person person = new Person();
+        person.passYears(20);
+        assertEquals(20, person.getAge());
     }
 
 }
