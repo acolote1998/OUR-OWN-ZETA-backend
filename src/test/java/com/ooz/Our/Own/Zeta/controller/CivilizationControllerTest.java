770c02cfcb60b22479f355667b050323c890d8b8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-20 11:16:53 +0100

    Test: gets a civilization that does not exist

--- src/test/java/com/ooz/Our/Own/Zeta/controller/CivilizationControllerTest.java
+++ src/test/java/com/ooz/Our/Own/Zeta/controller/CivilizationControllerTest.java
@@ -1,11 +1,15 @@
 package com.ooz.Our.Own.Zeta.controller;
 
+import com.ooz.Our.Own.Zeta.model.Civilization;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +20,17 @@ class CivilizationControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Test
+    @Order(1)
+    void gettingCivilizationThatDoesNotExist() {
+        // Llamada a la API con un nombre que no existe
+        ResponseEntity<String> response = restTemplate.getForEntity("/civilizations/Aztecs", String.class);
+
+        // Verificar que la respuesta sea un error 500
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+
+        // Opcional: Verificar que el cuerpo de la respuesta contenga un mensaje de error
+        assertTrue(response.getBody().contains("Civilization not found"));
+    }
 
 }
