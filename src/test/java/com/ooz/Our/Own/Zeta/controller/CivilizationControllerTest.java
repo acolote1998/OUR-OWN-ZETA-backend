9ec84a121267326380175bfb51738265a6b7e218
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-20 10:53:57 +0100

    Civ. Controller Test Initialized

---
+++ src/test/java/com/ooz/Our/Own/Zeta/controller/CivilizationControllerTest.java
@@ -0,0 +1,20 @@
+package com.ooz.Our.Own.Zeta.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CivilizationControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+
+}
