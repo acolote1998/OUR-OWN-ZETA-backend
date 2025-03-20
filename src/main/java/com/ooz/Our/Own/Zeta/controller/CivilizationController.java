29f4ce2cd54074ce03118c381f3250c5a40c72ca
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-20 10:37:10 +0100

    Controller: get civilization by name

--- src/main/java/com/ooz/Our/Own/Zeta/controller/CivilizationController.java
+++ src/main/java/com/ooz/Our/Own/Zeta/controller/CivilizationController.java
@@ -1,9 +1,32 @@
 package com.ooz.Our.Own.Zeta.controller;
 
+import com.ooz.Our.Own.Zeta.model.Civilization;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/civilization?")
+@RequestMapping("/civilizations")
 public class CivilizationController {
+
+    private final Map<String, Civilization> civilizations = new HashMap<>();
+
+    // Initializing some civilizations in the future maybe
+    public CivilizationController() {
+    }
+
+    // Obtaining civilization by name
+    // GET http://localhost:8080/civlizations/name
+    @GetMapping("/{name}")
+    public Civilization getCivilization(@PathVariable String name) {
+        Civilization civilization = civilizations.get(name);
+        if (civilization == null) {
+            throw new RuntimeException("Civilization not found");
+        }
+        return civilization;
+    }
 }
