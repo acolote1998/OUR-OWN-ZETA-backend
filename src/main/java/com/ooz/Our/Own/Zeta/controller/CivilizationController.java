31b48c1e9fc68228ada706c5294267ccc05bfd20
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-20 10:21:19 +0100

    Civilization controller created

---
+++ src/main/java/com/ooz/Our/Own/Zeta/controller/CivilizationController.java
@@ -0,0 +1,9 @@
+package com.ooz.Our.Own.Zeta.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/civilization?")
+public class CivilizationController {
+}
